8e4c8b74705572aa8eaccd43d78151e3e91c98ef
Amina <107080579+Aminalo@users.noreply.github.com>
2025-08-11 15:36:03 -0400

    part 2 done

--- scrip.js
+++ scrip.js
@@ -1,3 +1,6 @@
+// ===== Part 1 =====
+
+
 // 1. Select and cache the <main> element
 const mainEl = document.querySelector('main');
 
@@ -11,3 +14,16 @@ mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
 mainEl.classList.add("flex-ctr");
 
 
+// ===== Part 2 =====
+
+// 1. Select and cache the <nav id="top-menu"> element
+const topMenuEl = document.getElementById("top-menu");
+
+// 2. Set height to 100%
+topMenuEl.style.height = "100%";
+
+// 3. Set background color using CSS custom property
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+// 4. Add the flex-around class
+topMenuEl.classList.add("flex-around");
